31c374c63e8e44ac6e6b87e7d0cdb4937fe4b373
Ho Kim <besqer996@gnu.ac.kr>
2019-03-31 00:35:03 +0900

    Add UI events for MapViewActivity
    - Modify some UI codes in MapViewActivity
    - Fix some bugs

--- app/src/main/java/com/levelup/Questrip/quest/QuestMapActivity.java
+++ app/src/main/java/com/levelup/Questrip/quest/QuestMapActivity.java
@@ -2,15 +2,21 @@ package com.levelup.Questrip.quest;
 
 import android.content.Intent;
 import android.os.Handler;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
 
 import com.levelup.Questrip.R;
 import com.levelup.Questrip.about.AboutActivity;
 import com.levelup.Questrip.common.CommonAlert;
 import com.levelup.Questrip.common.QuestManager;
 import com.levelup.Questrip.config.ConfigActivity;
+import com.levelup.Questrip.data.Account;
 import com.levelup.Questrip.net.ClientRequestAsync;
 
 /**
@@ -31,16 +37,40 @@ import com.levelup.Questrip.net.ClientRequestAsync;
  * 리더보드 버튼 - 리더보드 보기 화면으로 이동합니다.
  * @see com.levelup.Questrip.view.LeaderBoardActivity
  */
-public final class QuestMapActivity extends FragmentActivity {
+public final class QuestMapActivity extends FragmentActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private MapViewManager manager;
+
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quest_map);
+        init();
+    }
+
+    /**
+     * 변수 및 필드를 초기화합니다.
+     */
+    private void init() {
         // SupportMapFragment 객체를 획득한 후, 지도가 사용 가능한 지 검사합니다.
         manager = new MapViewManager(this, this::onMapReady);
+
+        // 필드
+        drawerLayout = findViewById(R.id.quest_map);
+        navigationView = drawerLayout.findViewById(R.id.quest_map_nav_menu);
+
+        // 닉네임, 집주소 추가
+        View header = navigationView.getHeaderView(0);
+        TextView fieldNickname = header.findViewById(R.id.quest_map_nickname);
+        fieldNickname.setText(Account.getInstance().getNickname());
+        TextView fieldAddress = header.findViewById(R.id.quest_map_address);
+        fieldAddress.setText(Account.getInstance().getAddress());
+
+        // 이벤트
+        navigationView.setNavigationItemSelectedListener(this);
     }
 
     /**
@@ -82,10 +112,8 @@ public final class QuestMapActivity extends FragmentActivity {
     /**
      * 사용자 정보 보기 버튼을 눌렀을 경우의 이벤트입니다.
      * 사용자 정보 액티비티로 이동합니다.
-     * @param view 사용자 정보 보기 버튼
      */
-    public void onClickAbout(View view) {
-        // TODO 버튼 디자인
+    private void showAbout() {
         Intent intent = new Intent(getApplicationContext(), AboutActivity.class);
         startActivity(intent);
     }
@@ -93,20 +121,73 @@ public final class QuestMapActivity extends FragmentActivity {
     /**
      * 환경설정 버튼을 눌렀을 경우의 이벤트입니다.
      * 환경설정 액티비티로 이동합니다.
-     * @param view 환경설정 버튼
      */
-    public void onClickConfig(View view) {
-        // TODO 버튼 디자인
+    private void showConfig() {
         Intent intent = new Intent(getApplicationContext(), ConfigActivity.class);
         startActivity(intent);
     }
 
     /**
-     * 뒤로가기 버튼을 누른 경우, 앱을 종료할 것인지 물어봅니다.
+     * 메뉴창이 열려있는 지 검사합니다.
+     * @return 메뉴창이 열려있다면 true 를 반환합니다.
+     */
+    private boolean isMenuOpen() {
+        return drawerLayout.isDrawerOpen(navigationView);
+    }
+
+    /**
+     * 메뉴창을 엽니다.
+     */
+    private void openMenu() {
+        drawerLayout.openDrawer(navigationView);
+    }
+
+    /**
+     * 메뉴창을 닫습니다.
+     */
+    private void closeMenu() {
+        drawerLayout.closeDrawer(navigationView);
+    }
+
+    /**
+     * 메뉴 버튼을 눌렀을 경우의 이벤트입니다.
+     * 메뉴를 보여줍니다.
+     * @param view 메뉴 버튼
+     */
+    public void onToggleMenu(View view) {
+        openMenu();
+    }
+
+    /**
+     * 메뉴의 어떤 항목을 선택한 경우의 이벤트입니다.
+     * 메뉴창을 닫고, 각 항목의 역할을 수행합니다.
+     * @param item 항목
+     * @return 항목이 선택된 경우의 이펙트를 적용하려면 true 를 반환합니다.
+     */
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        // 일단 메뉴는 닫습니다.
+        closeMenu();
+        // 각 항목의 역할을 수행합니다.
+        switch (item.getItemId()) {
+            case R.id.quest_map_item_about:
+                showAbout();
+                break;
+            case R.id.quest_map_item_config:
+                showConfig();
+                break;
+        }
+        return false;
+    }
+
+    /**
+     * 뒤로가기 버튼을 누른 경우, 메뉴창을 닫거나 앱을 종료할 것인지 물어봅니다.
      */
     @Override
     public void onBackPressed() {
-        CommonAlert.closeApp(this);
+        // 메뉴창이 열려있는 경우, 메뉴창을 닫습니다.
+        if (isMenuOpen()) closeMenu();
+        // 메뉴창이 닫혀있는 경우, 앱을 종료할 것인지 물어봅니다.
+        else CommonAlert.closeApp(this);
     }
 
 }
